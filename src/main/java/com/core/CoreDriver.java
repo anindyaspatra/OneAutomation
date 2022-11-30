dc58a74d2472fddb5ac5356d8c89ce8c7e604c42
anindyaspatra <anindyasundar.patra@gmail.com>
2022-11-30 11:43:13 +0530

    Updated with gradle.properties file

--- src/main/java/com/core/CoreDriver.java
+++ src/main/java/com/core/CoreDriver.java
@@ -2,9 +2,21 @@ package com.core;
 
 import org.openqa.selenium.WebDriver;
 
+import java.util.HashMap;
+
 public class CoreDriver {
+//Static Final Keywords for reading data from config.properties file
+    private static final String BROWSER = "browser";
+    private static final String URL = "url";
+    private static final String PAGE_LOAD_TIMEOUT = "pageloadtimeout";
+    private static final String CHROME = "Chrome";
+    private static final String FIREFOX = "Firefox";
+    private static final String IEXPLORER = "InternetExplorer";
+    private static final String SAFARI = "Safari";
+    private static final String HEADLESS = "headless";
 
     public static WebDriver driver;
+    public static HashMap<String, String> configProp;
 
 
 }
